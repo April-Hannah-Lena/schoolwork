ccaa2b5b5340c24429efd4a1ed63bb39617fbf6f
April-Hannah-Lena <aprillherwig@gmail.com>
2021-01-12 08:00:25 +0100

    fixed typo

--- VectorField.jl
+++ VectorField.jl
@@ -19,7 +19,7 @@ end
 # points is an Array of size dx2, each row represents a point in space 
 # vectorfield2d(field::Function, points <: AbstractArray{<:Number,2}, arrowscale::Real=0.1)
 
-function vectorfield!(points, field::Funtion; arrowscale=0.1)
+function vectorfield!(points, field::Function; arrowscale=0.1)
     vectors = similar(points)
     d = size(points, 2)
     for i in 1:d
@@ -59,13 +59,12 @@ function vectorfield(xs, ys, u::T, v::T; arrowscale=0.1) where {T <: AbstractArr
     d = size(points, 2)
     d == length(u) == length(v) || error("Dimensions do not match. Ensure length(u)==length(v)==length(xs)*length(ys)")
     vectors = [reshape(u', 1, :); reshape(v', 1, :)]
-    for i in 1:d
-        vectors[:, i] .= collect(field(points[:, i]...))
-    end
     vectors .*= arrowscale
     quiver(points[1, :], points[2, :], quiver=(vectors[1, :], vectors[2, :]))
 end
-# u, v are 2 matrices. Grid will be interpolated as:
+# u, v are 2 matrices, the value of u[i, j] is the x component of the arrow, 
+# the value of v[i, j] is the y component of the arrow
+# Grid will be interpolated as:
 #     ys - >           ys - >
 # xs                xs
 # |    u            |    v
@@ -76,9 +75,34 @@ function vectorfield!(xs, ys, u::T, v::T; arrowscale=0.1) where {T <: AbstractAr
     d = size(points, 2)
     d == length(u) == length(v) || error("Dimensions do not match. Ensure length(u)==length(v)==length(xs)*length(ys)")
     vectors = [reshape(u', 1, :); reshape(v', 1, :)]
-    for i in 1:d
-        vectors[:, i] .= collect(field(points[:, i]...))
-    end
+    vectors .*= arrowscale
+    quiver!(points[1, :], points[2, :], quiver=(vectors[1, :], vectors[2, :]))
+end
+
+function vectorfield(xs, ys, uv::T; arrowscale=0.1) where {T <: AbstractArray{S,N} where S where N}
+    points = meshgrid(xs, ys)
+    d = size(points, 2)
+    u = [uv[i][1] for i in CartesianIndices(uv)]
+    v = [uv[i][2] for i in CartesianIndices(uv)]
+    d == length(u) == length(v) || error("Dimensions do not match. Ensure length(u)==length(v)==length(xs)*length(ys)")
+    vectors = [reshape(u', 1, :); reshape(v', 1, :)]
+    vectors .*= arrowscale
+    quiver(points[1, :], points[2, :], quiver=(vectors[1, :], vectors[2, :]))
+end
+# uv is a matrix of 2-element arrays [x, y]. Arrows will be interpreted componentwise.
+# Grid will be interpolated as:
+#     ys - >
+# xs
+# |    uv
+# v
+
+function vectorfield!(xs, ys, uv::T; arrowscale=0.1) where {T <: AbstractArray{S,N} where S where N}
+    points = meshgrid(xs, ys)
+    d = size(points, 2)
+    u = [uv[i][1] for i in CartesianIndices(uv)]
+    v = [uv[i][2] for i in CartesianIndices(uv)]
+    d == length(u) == length(v) || error("Dimensions do not match. Ensure length(u)==length(v)==length(xs)*length(ys)")
+    vectors = [reshape(u', 1, :); reshape(v', 1, :)]
     vectors .*= arrowscale
     quiver!(points[1, :], points[2, :], quiver=(vectors[1, :], vectors[2, :]))
 end
