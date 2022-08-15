28dde74ea8e3167fb3b1ce5f2da3ba0a81686563
April Herwig <aprillherwig@gmail.com>
2022-08-15 09:21:11 +0200

    detailed benchmark plots

--- Thesis/benchmarks/benchmark_launcher.jl
+++ Thesis/benchmarks/benchmark_launcher.jl
@@ -1,11 +1,4 @@
-using Plots, ProgressMeter, Serialization
-
-default(
-    fontfamily="computer modern",
-    linewidth=2, 
-    framestyle=:box
-)
-#Plots.scalefontsizes(1.3)
+using ProgressMeter, Serialization
 
 
 types  = ["nothing", "cpu", "gpu"]
@@ -20,37 +13,6 @@ times = @showprogress([
 serialize("times_points.jls", times)
 
 
-p = plot(
-    points,
-    [times[t, 1, :] for t in 1:length(types)],
-    xaxis=("Number of points", :log),
-    yaxis=("Time (s)", :log),
-    title="Execution Time for Simple Map",
-    lab=permutedims(["None", "CPU", "GPU"])
-);
-savefig(p, "times_simple.pdf")
-
-p = plot(
-    points,
-    [times[t, 2, :] for t in 1:length(types)],
-    xaxis=("Number of points", :log),
-    yaxis=("Time (s)", :log),
-    title="Execution Time for Medium Map",
-    lab=permutedims(["None", "CPU", "GPU"])
-);
-savefig(p, "times_medium.pdf")
-
-p = plot(
-    points,
-    [times[t, 3, :] for t in 1:length(types)],
-    xaxis=("Number of points", :log),
-    yaxis=("Time (s)", :log),
-    title="Execution Time for Complex Map",
-    lab=permutedims(["None", "CPU", "GPU"])
-);
-savefig(p, "times_complex.pdf")
-
-
 steps = ceil.(Int, 10 .^ (0.6:0.1:5.0))
 
 times = @showprogress([
@@ -59,15 +21,3 @@ times = @showprogress([
 ])
 
 serialize("times_complexity.jls", times)
-
-
-p = plot(
-    steps,
-    [times[t, :] for t in 1:length(types)],
-    xaxis=("Number of steps", :log),
-    yaxis=("Time (s)", :log),
-    title="Map Complexity vs Execution Time",
-    lab=permutedims(["None", "CPU", "GPU"])
-);
-savefig(p, "times_steps.pdf")
-
---
+++ Thesis/benchmarks/benchmark_plots.jl
@@ -0,0 +1,72 @@
+using Plots, Serialization
+
+default(
+    fontfamily="computer modern",
+    linewidth=2, 
+    framestyle=:box
+)
+#Plots.scalefontsizes(1.3)
+
+
+types  = ["nothing", "cpu", "gpu"]
+steps  = ceil.(Int, 10 .^ (1.0:3.0))
+points = 4 .* ceil.(Int, 10 .^ (1.0:0.2:3.0))
+times  = deserialize("times_points.jls")
+
+
+p = plot(
+    points,
+    [times[t, 1, :] for t in 1:length(types)],
+    #size=(900,600),
+    xaxis=("Number of points", :log),
+    yaxis=("Time (s)", :log),
+    #title="Execution Time for Simple Map",
+    legend_title="Acceleration",
+    legend_position=:topleft,
+    lab=permutedims(["None", "CPU", "GPU"])
+)
+savefig(p, "times_simple_loglog.pdf")
+
+p = plot(
+    points,
+    [times[t, 2, :] for t in 1:length(types)],
+    #size=(900,600),
+    xaxis=("Number of points", :log),
+    yaxis=("Time (s)", :log),
+    #title="Execution Time for Medium Map",
+    legend_title="Acceleration",
+    legend_position=:topleft,
+    lab=permutedims(["None", "CPU", "GPU"])
+)
+savefig(p, "times_medium_loglog.pdf")
+
+p = plot(
+    points,
+    [times[t, 3, :] for t in 1:length(types)],
+    #size=(900,600),
+    xaxis=("Number of points", :log),
+    yaxis=("Time (s)", :log),
+    #title="Execution Time for Complex Map",
+    legend_title="Acceleration",
+    legend_position=:topleft,
+    lab=permutedims(["None", "CPU", "GPU"])
+)
+savefig(p, "times_complex_loglog.pdf")
+
+
+steps = ceil.(Int, 10 .^ (0.6:0.1:5.0))
+times = deserialize("times_complexity.jls")
+
+
+p = plot(
+    steps,
+    [times[t, :] for t in 1:length(types)],
+    #size=(900,600),
+    xaxis=("Number of steps", :log),
+    yaxis=("Time (s)", :log),
+    #title="Map Complexity vs Execution Time",
+    legend_title="Acceleration",
+    legend_position=:topleft,
+    lab=permutedims(["None", "CPU", "GPU"])
+)
+savefig(p, "times_steps_loglog.pdf")
