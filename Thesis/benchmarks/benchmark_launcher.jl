b37ec7c7641cad1c48aa3eb55e412d60c1397da2
April-Hannah-Lena <aprillherwig@gmail.com>
2022-08-31 21:58:48 +0200

    thomas atttractor benchmark set up

--- Thesis/benchmarks/benchmark_launcher.jl
+++ Thesis/benchmarks/benchmark_launcher.jl
@@ -1,5 +1,7 @@
-using ProgressMeter, Serialization
+using ProgressMeter, Serialization, SMTPClient, Dates
+exit_code = false
 
+try
 
 types  = ["nothing", "cpu", "gpu"]
 steps  = ceil.(Int, 10 .^ (1.0:3.0))
@@ -21,3 +23,31 @@ times = @showprogress([
 ])
 
 serialize("times_complexity.jls", times)
+
+catch err
+
+exit_code = true
+
+finally
+
+include("uname+passwd.jl")
+
+opt = SendOptions(
+    isSSL = true,
+    username = username,
+    passwd = passwd
+)
+
+body = IOBuffer(
+    "Date: $(Dates.format(Dates.now(), "e, dd u yyyy HH:MM:SS")) +0100\r\n" *
+    "From: Me <$(username)>\r\n" *
+    "To: $(rcpt)\r\n" *
+    "Subject: Benchmark $(exit_code ? "un" : "")successfully finished\r\n" *
+    "\r\n" *
+    "Go turn off the computer.\r\n"
+)
+
+url = "smtps://smtp.gmail.com:465"
+resp = send(url, ["<$(rcpt)>"], "<$(username)>", body, opt)
+
+end
--- Thesis/benchmarks/boxmap_benchmark.jl
+++ Thesis/benchmarks/boxmap_benchmark.jl
@@ -6,14 +6,14 @@ const step_size = Float32(0.1f0 / steps)
 const points = Int32(Meta.parse(ARGS[3]))
 
 # the Lorenz system
-const σ, ρ, β = 10f0, 28f0, 0.4f0
+const b = 0.208186
 
-v((x,y,z)) = (σ*(y-x), ρ*x-y-x*z, x*y-β*z)
+v((x,y,z)) = (sin(y)-b*x, sin(z)-b*y, sin(x)-b*z)
 f(x) = rk4_flow_map(v, x, step_size, steps)
 
-center, radius = Float32.((0,0,25)), Int32.((30,30,30))
+center, radius = Float32.((0,0,0)), Int32.((5,5,5))
 P = BoxPartition(Box(center, radius), Int32.((128,128,128)))
-x = (sqrt(β*(ρ-1)), sqrt(β*(ρ-1)), ρ-1)         # equilibrium
+x = zeros(3)
 S = P[x]
 
 F = BoxMap(f, P, type, no_of_points=points)
